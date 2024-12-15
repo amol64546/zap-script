85ab3b534f4959045bcbb557995e9b0c2d6325e5
Amol Nakhate <106758417+amol64546@users.noreply.github.com>
2024-12-15 16:20:40 +0530

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -11,10 +11,6 @@ ZAP_API_KEY="gaianmobius"
 ZAP_HOST="https://zap.aidtaas.com"
 OPENAPI_URL="$SWAGGER_DOC_URL"
 
-path=$(echo "$OPENAPI_URL" | awk -F"${domain}" '{print $2}')
-
-FILTER_URI=$(echo "$path" | awk -F'/' '{print $2}')
-
 # Import OpenAPI definition
 import_response=$(curl -s -G "${ZAP_HOST}/JSON/openapi/action/importUrl/" \
   --data-urlencode "url=${OPENAPI_URL}" \
@@ -43,13 +39,4 @@ add_id_to_json() {
 
 updated_json=$(add_id_to_json "$formatted_report")
 
-# Filter the report to include only instances with specific URI
-filtered_report=$(echo "$updated_json" | jq --arg filter_uri "$FILTER_URI" '
-  .site |= map(
-    .alerts |= map(
-      .instances |= map(select(.uri | contains($filter_uri)))
-    )
-  )
-')
-
-echo "$filtered_report"
+echo "$updated_json"
