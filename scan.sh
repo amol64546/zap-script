4c300610a88d795290f4e5e2a5aab3a6d684dd2e
Amol Nakhate <106758417+amol64546@users.noreply.github.com>
2024-12-15 15:55:04 +0530

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -1,13 +1,49 @@
-# Step 1: Generate a random file name
-RANDOM_FILENAME="zap_report_$(date +%s)_$(shuf -i 1000-9999 -n 1).json"
+#!/bin/bash
 
-# Step 2: Run the ZAP scan with the generated random file name, silencing all other output
-docker run -v /root/zap:/zap/wrk/:rw -t zaproxy/zap-stable:latest \
-  zap-api-scan.py -t $SWAGGER_DOC_URL -f openapi -J /zap/wrk/"$RANDOM_FILENAME" \
-  > /dev/null 2>&1
+# Configuration
+ZAP_API_KEY="gaianmobius"
+ZAP_HOST="https://zap.aidtaas.com"
+OPENAPI_URL="$SWAGGER_DOC_URL"
 
-# Step 3: Echo the content of the report file (only the content)
-echo "$(cat /root/zap/$RANDOM_FILENAME)"
+path=$(echo "$OPENAPI_URL" | awk -F"${domain}" '{print $2}')
 
-# Step 4: Remove the report file after use
-rm /root/zap/"$RANDOM_FILENAME"
+FILTER_URI=$(echo "$path" | awk -F'/' '{print $2}')
+
+# Import OpenAPI definition
+import_response=$(curl -s -G "${ZAP_HOST}/JSON/openapi/action/importUrl/" \
+  --data-urlencode "url=${OPENAPI_URL}" \
+  --data-urlencode "apikey=${ZAP_API_KEY}")
+
+# Generate JSON report
+report=$(curl -s -G "${ZAP_HOST}/OTHER/core/other/jsonreport/" \
+  --data-urlencode "apikey=${ZAP_API_KEY}")
+
+# Modify field names in the report
+formatted_report=$(echo "$report" | jq 'with_entries(if .key[0:1] == "@" then .key |= .[1:] else . end)')
+
+
+# Function to add a random "id" field to JSON
+add_id_to_json() {
+  local input_json="$1"
+  
+  # Generate a random alphanumeric ID
+  random_id=$(uuidgen | tr -d '-' | head -c 9)
+
+  # Add the "id" field to the JSON using jq
+  updated_json=$(echo "$input_json" | jq --arg id "$random_id" '. + {id: $id}')
+  
+  echo "$updated_json"
+}
+
+updated_json=$(add_id_to_json "$formatted_report")
+
+# Filter the report to include only instances with specific URI
+filtered_report=$(echo "$updated_json" | jq --arg filter_uri "$FILTER_URI" '
+  .site |= map(
+    .alerts |= map(
+      .instances |= map(select(.uri | contains($filter_uri)))
+    )
+  )
+')
+
+echo "$filtered_report"
